4a9b5b29827420c5f39a9732d883bc700828733c
Nigamananda Joshi <136053701+nigamanandajoshi@users.noreply.github.com>
2025-06-15 23:39:05 +0530

    Update index.tsx

--- src/views/index.tsx
+++ src/views/index.tsx
@@ -3,7 +3,7 @@ export {ToolView} from "./tools";
 export {FeatureView} from "./feature";
 export {OfferView} from "./offer";
 export {FaqView} from "./faq";
-// export {AirdropView} from "./airdrop";
+export {AirdropView} from "./airdrop";
 // export {DonateView} from "./donate";
 export {CreateView} from "./create";
 // export {InputView} from "./input";
