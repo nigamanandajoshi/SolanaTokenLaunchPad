ab81b958a2313ab38eb427213ddbf6fc64a822b1
Nigamananda Joshi <136053701+nigamanandajoshi@users.noreply.github.com>
2025-06-15 18:08:01 +0530

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,7 +14,7 @@ const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <div className="bg-default-900">
       <Head>
-        <title>Solana Token Creator</title>
+        <title>Solana Token Lunchpad</title>
       </Head>
       <ContextProvider>
         <AppBar />
